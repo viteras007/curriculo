d36a7bc18793e6704635c5d4f49619a21c8d8964
viteras007 <viteras__@hotmail.com>
2019-04-04 20:36:04 -0300

    Updates

---
+++ precache-manifest.2b86dca499ed21841dbe7aaedc5009c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0f0d129290c9eb361483",
+    "url": "/curriculo/static/js/runtime~main.b71702cc.js"
+  },
+  {
+    "revision": "7d35fc2a45f8d8628810",
+    "url": "/curriculo/static/js/main.47957a9b.chunk.js"
+  },
+  {
+    "revision": "558287cbe6c421484fdb",
+    "url": "/curriculo/static/js/2.258c28fe.chunk.js"
+  },
+  {
+    "revision": "7d35fc2a45f8d8628810",
+    "url": "/curriculo/static/css/main.91a6f48f.chunk.css"
+  },
+  {
+    "revision": "558287cbe6c421484fdb",
+    "url": "/curriculo/static/css/2.5e581853.chunk.css"
+  },
+  {
+    "revision": "8e351cbf1bf5db1771e580e656210566",
+    "url": "/curriculo/index.html"
+  }
+];
